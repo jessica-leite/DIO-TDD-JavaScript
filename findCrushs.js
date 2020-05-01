41f92726fbcc51583742570907f2a798ed053093
Jessica <jessica@email.com>
2020-05-01 11:07:04 +0000

    Error tests created

--- findCrushs.js
+++ findCrushs.js
@@ -1,5 +1,17 @@
 const somaCrush = (n1, n2) => n1 + n2;
-const encontraCrush = () => "Crush não encontrado";
+const encontraCrush = nome => {
+    try {
+        validacao(nome)
+        return 'Crush não encontrado';
+    } catch (errorMessage) {
+        return errorMessage;
+    }
+}
+
+const validacao = nome => {
+    if (!nome) throw 'Campo nome está vazio';
+    if (typeof nome !== 'string') throw 'Tipo inválido';
+}
 
 module.exports = {
     somaCrush,
--- tests/findCrushListTest.js
+++ tests/findCrushListTest.js
@@ -3,12 +3,18 @@ const findCrushs = require('../findCrushs');
 
 describe('find crushs', () => {
     it('soma crushs', () => {
-        findCrushs.somaCrush(2,3).should.be.equal(5)
-    })
+        findCrushs.somaCrush(2, 3).should.be.equal(5)
+    });
     it('somar dois números e retornar 30', () => {
-        findCrushs.somaCrush(10,20).should.be.equal(30)
-    })
+        findCrushs.somaCrush(10, 20).should.be.equal(30)
+    });
     it('Passar um nome e deve retornar mensagem de pessoa não encontrada', () => {
         findCrushs.encontraCrush('Joselito Marques').should.be.equal('Crush não encontrado')
-    })
+    });
+    it('Retorna mensagem de campo nome está vazio', () => {
+        findCrushs.encontraCrush().should.be.equal('Campo nome está vazio')
+    });
+    it('Retorna mensagem de tipo inválido', () => {
+        findCrushs.encontraCrush(123).should.be.equal('Tipo inválido')
+    });
 })
